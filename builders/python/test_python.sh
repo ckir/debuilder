6f2a172efbe418dadef9d3931d1c7beedc7a649f
Costas Kirgoussios <ckir1508@hotmail.com>
2023-06-13 05:34:40 +0300

    Initial commit

---
+++ builders/python/test_python.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rm -Rf ./tmp/*
+docker rmi $(docker images -qa -f 'dangling=true') 2> /dev/null
+docker build -t python .
+docker run --rm -it \
+-e AWS_REGION=$AWS_REGION \
+-e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+-e AWS_SECRET_KEY=$AWS_SECRET_KEY \
+--mount type=bind,source="$(pwd)"/tmp,target=/Release \
+--entrypoint bash python
